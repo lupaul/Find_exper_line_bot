e650b4e7e55c090518e92c8f3707528294305b43
pochia <lupochia@gmail.com>
2018-01-31 14:09:38 +0800

    change ai to olami ai api

--- app/services/ai/conversation.rb
+++ app/services/ai/conversation.rb
@@ -1,5 +1,6 @@
 module Ai::Conversation
   require 'net/http'
+  require 'digest'
 
   def self.get_response(message,session_id)
     _get_response(message,session_id)
@@ -8,12 +9,30 @@ module Ai::Conversation
   private
 
   def self._get_response(message,session_id)
-    token = Rails.application.config_for(:api_key)["api_ai"]["token"]
+    timestamp = (Time.now.to_i).to_s + "000"
     api_url = Rails.application.config_for(:api_key)["api_ai"]["api_url"]
+    key = Rails.application.config_for(:api_key)["api_ai"]["key"]
+    secret = Rails.application.config_for(:api_key)["api_ai"]["secret"]
+    sign_key = Digest::MD5.hexdigest "#{secret}api=nliappkey=#{key}timestamp=#{timestamp}#{secret}"
     uri = URI(api_url)
-    res = JSON.parse(Net::HTTP.post_form(uri,token: token, query: message, session_id: session_id).body)["answer"]
+    response = Net::HTTP.post_form(uri,,appkey: key,
+              api: "nli",
+              timestamp: timestamp,
+              sign: sign_key,
+              rq: "{'data':{'input_type':1,'text': #{message}},'data_type':'stt'}",
+              cusid: session_id)
+    res = JSON.parse(response.body)["data"]["nli"][0]["desc_obj"]["result"]
     return res
 
   end
+
+  # def self._get_response(message,session_id)
+  #   token = Rails.application.config_for(:api_key)["api_ai"]["token"]
+  #   api_url = Rails.application.config_for(:api_key)["api_ai"]["api_url"]
+  #   uri = URI(api_url)
+  #   res = JSON.parse(Net::HTTP.post_form(uri,token: token, query: message, session_id: session_id).body)["answer"]
+  #   return res
+  #
+  # end
 
 end
